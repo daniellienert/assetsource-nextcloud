ee5b2373f1cd1b6d8783424b458a1c5c3d9e92f2
Daniel Lienert <daniel@lienert.cc>
2019-03-10 17:14:46 +0100

    TASK: Change document license header

--- Classes/NextCloudApi/WebDav/Dto/SearchResult.php
+++ Classes/NextCloudApi/WebDav/Dto/SearchResult.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 namespace DL\AssetSource\NextCloud\NextCloudApi\WebDav\Dto;
 
 /*
- *  (c) 2019 punkt.de GmbH - Karlsruhe, Germany - http://punkt.de
- *  All rights reserved.
+ * This file is part of the DL.AssetSource.NextCloud package.
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
  */
 
 class SearchResult
